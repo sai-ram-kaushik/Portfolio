43ebb81bf3d42efcea3c6dd3fba91dc46f820deb
sai-ram-kaushik <sairamkaushik1@gmail.com>
2024-04-08 20:10:53 +0530

    updated with the boiler plate code

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,14 +3,20 @@ module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./ui/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        background: "#040619",
+        primary: "#ffffff",
+        secondary: "#4b6bfb",
+      },
+
+      fontFamily: {
+        title: "Volkhov",
+        bodyContent: "Inter Tight",
       },
     },
   },
---
+++ ui/Sidebar.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Sidebar = () => {
+  return (
+    <div className="min-w-[500px] h-full">
+      <div className="flex flex-col items-center justify-center w-full h-full border-r">
+        
+      </div>
+    </div>
+  );
+};
+
+export default Sidebar;
